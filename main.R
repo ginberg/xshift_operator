86a89816661a46c530dec45ab6358fb1363e722a
Ger Inberg <info@gerinberg.com>
2021-04-14 18:51:25 +0000

    test

--- main.R
+++ main.R
@@ -3,68 +3,74 @@ library(tercen)
 library(dplyr)
 library(flowCore)
 
-fcs_to_data = function(filename) {
-  data_fcs = read.FCS(filename, transformation = FALSE)
-  names_parameters = data_fcs@parameters@data$desc
-  data = as.data.frame(exprs(data_fcs))
-  col_names = colnames(data)
-  names_parameters = ifelse(is.na(names_parameters),col_names,names_parameters)
-  colnames(data) = names_parameters
-  data %>%
-    mutate_if(is.logical, as.character) %>%
-    mutate_if(is.integer, as.double) %>%
-    mutate(.ci = rep_len(0, nrow(.))) %>%
-    mutate(filename = rep_len(basename(filename), nrow(.)))
-}
+# fcs_to_data = function(filename) {
+#   data_fcs = read.FCS(filename, transformation = FALSE)
+#   names_parameters = data_fcs@parameters@data$desc
+#   data = as.data.frame(exprs(data_fcs))
+#   col_names = colnames(data)
+#   names_parameters = ifelse(is.na(names_parameters),col_names,names_parameters)
+#   colnames(data) = names_parameters
+#   data %>%
+#     mutate_if(is.logical, as.character) %>%
+#     mutate_if(is.integer, as.double) %>%
+#     mutate(.ci = rep_len(0, nrow(.))) %>%
+#     mutate(filename = rep_len(basename(filename), nrow(.)))
+# }
+# 
+# ctx = tercenCtx()
+# 
+# if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+# 
+# #1. extract files
+# df <- ctx$cselect()
+# 
+# docId = df$documentId[1]
+# doc = ctx$client$fileService$get(docId)
+# filename = tempfile()
+# writeBin(ctx$client$fileService$download(docId), filename)
+# on.exit(unlink(filename))
+# 
+# # unzip if archive
+# if(length(grep(".zip", doc$name)) > 0) {
+#   tmpdir <- tempfile()
+#   unzip(filename, exdir = tmpdir)
+#   f.names <- list.files(tmpdir, full.names = TRUE)
+# } else {
+#   f.names <- filename
+# }
+# 
+# # check FCS
+# if(any(!isFCSfile(f.names))) stop("Not all imported files are FCS files.")
+# 
+# assign("actual", 0, envir = .GlobalEnv)
+# task = ctx$task
+# 
+# 
+# #2. convert them to FCS files
+# f.names %>%
+#   lapply(function(filename){
+#     data = fcs_to_data(filename)
+#     if (!is.null(task)) {
+#       # task is null when run from RStudio
+#       actual = get("actual",  envir = .GlobalEnv) + 1
+#       assign("actual", actual, envir = .GlobalEnv)
+#       evt = TaskProgressEvent$new()
+#       evt$taskId = task$id
+#       evt$total = length(f.names)
+#       evt$actual = actual
+#       evt$message = paste0('processing FCS file ' , filename)
+#       ctx$client$eventService$sendChannel(task$channelId, evt)
+#     } else {
+#       cat('processing FCS file ' , filename)
+#     }
+#     data
+#   }) %>%
+#   bind_rows() %>%
+#   ctx$addNamespace() %>%
+#   ctx$save()
 
-ctx = tercenCtx()
+# TODO create input
 
-if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+system("java -Xmx32G -cp VorteX.jar -Djava.awt.headless=true standalone.Xshift")
 
-#1. extract files
-df <- ctx$cselect()
-
-docId = df$documentId[1]
-doc = ctx$client$fileService$get(docId)
-filename = tempfile()
-writeBin(ctx$client$fileService$download(docId), filename)
-on.exit(unlink(filename))
-
-# unzip if archive
-if(length(grep(".zip", doc$name)) > 0) {
-  tmpdir <- tempfile()
-  unzip(filename, exdir = tmpdir)
-  f.names <- list.files(tmpdir, full.names = TRUE)
-} else {
-  f.names <- filename
-}
-
-# check FCS
-if(any(!isFCSfile(f.names))) stop("Not all imported files are FCS files.")
-
-assign("actual", 0, envir = .GlobalEnv)
-task = ctx$task
-
-
-#2. convert them to FCS files
-f.names %>%
-  lapply(function(filename){
-    data = fcs_to_data(filename)
-    if (!is.null(task)) {
-      # task is null when run from RStudio
-      actual = get("actual",  envir = .GlobalEnv) + 1
-      assign("actual", actual, envir = .GlobalEnv)
-      evt = TaskProgressEvent$new()
-      evt$taskId = task$id
-      evt$total = length(f.names)
-      evt$actual = actual
-      evt$message = paste0('processing FCS file ' , filename)
-      ctx$client$eventService$sendChannel(task$channelId, evt)
-    } else {
-      cat('processing FCS file ' , filename)
-    }
-    data
-  }) %>%
-  bind_rows() %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+# TODO read output and write to tercen
